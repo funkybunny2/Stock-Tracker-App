08b7fdc2b53fa0800ce75db20091cbeba38dc0c2
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2023-09-06 20:03:11 -0500

    added more informaiton

--- js/display.js
+++ js/display.js
@@ -22,14 +22,31 @@ function fetchStockData(symbol) {
     const apiUrl = `https://www.alphavantage.co/query?function=${functionName}&symbol=${symbol}&outputsize=${outputSize}&apikey=${apiKey}&datatype=${dataType}`;
 
     fetch(apiUrl)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch stock data.');
+            }
+            return response.json();
+        })
         .then(data => {
             displayStockData(data);
         })
         .catch(error => {
             console.error("Error fetching data:", error);
-            stockNameDisplay.textContent = "Error fetching data.";
+            stockInfo.innerHTML = "Error fetching data.";
+            // Clear previous data
+            clearStockData();
         });
+}
+// Function to clear stock data
+function clearStockData() {
+    stockNameDisplay.textContent = "";
+    stockSymbolDisplay.textContent = "";
+    stockPriceDisplay.textContent = "";
+    stockOpenDisplay.textContent = "";
+    stockHighDisplay.textContent = "";
+    stockLowDisplay.textContent = "";
+    stockVolumeDisplay.textContent = "";
 }
 // Function to display stock data
 function displayStockData(data) {
@@ -37,7 +54,14 @@ function displayStockData(data) {
     const timeSeries = data['Time Series (Daily)'];
 
     if (!metaData || !timeSeries) {
+        // Clear the stock data if no data is found
         stockNameDisplay.textContent = "Stock data not found.";
+        stockSymbolDisplay.textContent = "";
+        stockPriceDisplay.textContent = "";
+        stockOpenDisplay.textContent = "";
+        stockHighDisplay.textContent = "";
+        stockLowDisplay.textContent = "";
+        stockVolumeDisplay.textContent = "";
         return;
     }
 
@@ -47,14 +71,32 @@ function displayStockData(data) {
     
     const stockName = metaData['1. Information'];
     const stockPrice = dailyData['4. close'];
+    const stockOpen = dailyData['1. open'];
+    const stockHigh = dailyData['2. high'];
+    const stockLow = dailyData['3. low'];
+    const stockVolume = dailyData['5. volume'];
 
+    // Clear the stock data first
+    stockNameDisplay.textContent = "";
+    stockSymbolDisplay.textContent = "";
+    stockPriceDisplay.textContent = "";
+    stockOpenDisplay.textContent = "";
+    stockHighDisplay.textContent = "";
+    stockLowDisplay.textContent = "";
+    stockVolumeDisplay.textContent = "";
+
+    // Update with the new data
     stockNameDisplay.textContent = stockName;
     stockSymbolDisplay.textContent = `Symbol: ${symbol}`;
     stockPriceDisplay.textContent = `Price: $${stockPrice}`;
-    
-    // Clear the "Change" display as it's not available
-    stockChangeDisplay.textContent = "";
+    stockOpenDisplay.textContent = `Open: $${stockOpen}`;
+    stockHighDisplay.textContent = `High: $${stockHigh}`;
+    stockLowDisplay.textContent = `Low: $${stockLow}`;
+    stockVolumeDisplay.textContent = `Volume: ${stockVolume}`;
 }
+
+
+
 // Function to display stock data
 // function displayStockData(data) {
 //     const metaData = data['Meta Data'];
