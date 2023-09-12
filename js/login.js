b9926e00751fbbe2af3be336d19dce2d579d8200
funkybunny2 <100392730+funkybunny2@users.noreply.github.com>
2023-09-12 18:49:31 -0500

    trying to add login feature

---
+++ js/login.js
@@ -0,0 +1,13 @@
+// Get references to HTML elements
+const loginButton = document.getElementById("loginButton");
+const logoutButton = document.getElementById("logoutButton");
+
+// Event listener for the login button
+loginButton.addEventListener("click", () => {
+    
+});
+
+// Event listener for the logout button
+logoutButton.addEventListener("click", () => {
+   
+});
